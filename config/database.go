5512ca322a5dccdbadf776c8b1d86d1ccb49ca98
Glen Tagubase <glen.tagubase03@gmail.com>
2026-02-19 20:27:43 +0800

    use env variables for db

--- config/database.go
+++ config/database.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,16 +12,23 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
+	host := os.Getenv("MYSQLHOST")
+	port := os.Getenv("MYSQLPORT")
+	user := os.Getenv("MYSQLUSER")
+	password := os.Getenv("MYSQLPASSWORD")
+	dbname := os.Getenv("MYSQLDATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", user, password, host, port, dbname)
+
 	var err error
-	// Added ?parseTime=true to fix the time.Time scanning issue
-	DB, err = sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/student-portal?parseTime=true")
+	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	err = DB.Ping()
 	if err != nil {
-		log.Fatal("Database not connected")
+		log.Fatal("Database not connected: ", err)
 	}
 
 	log.Println("✅ MySQL Connected")
